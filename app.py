40ba2ba90e5a4b794caf4766cbcb2c7b5e0d34bb
Sacha Wharton <sacha@gmail.com>
2020-10-02 21:53:27 +0200

    Fixed indentation and typos Added additional modules Fixed order

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from waitress import serve
 import pandas as pd
 import os
 
 app = Flask(__name__)
 api = Api(app)
 
-
-class Places(Resources):
-    
-
-api.add_resource(Places, '/places')
 DATA = {
     'places':
         ['rome',
@@ -25,36 +21,46 @@ DATA = {
          'barcelona']
 }
 
-def get(self):
-    # return our data and 200 OK HTTP code
-    return {'data': DATA}, 200
-
-def post(self):
-    # parse request arguments
-    parser = reqparse.RequestParser()
-    parser.add_argument('location', required=True)
-    args = parser.parse_args()# check if we already have the location in places list
-    if args['location'] in DATA['places']:
-        # if we do, return 401 bad request
-        return {
-            'message': f"'{args['location']}' already exists."
-            }, 401
-    else:
-        # otherwise, add the new location to places
-        DATA['places'].append(args['location'])
+class Places(Resource):
+    def get(self):
+        # return our data and 200 OK HTTP code
         return {'data': DATA}, 200
 
-def delete(self):
-    # parse request arguments
-    parser = reqparse.RequestParser()
-    parser.add_argument('location', required=True)
-    args = parser.parse_args()# check if we have given location in places list
-    if args['location'] in DATA['places']:
-        # if we do, remove and return data with 200 OK
-        DATA['places'].remove(args['location'])
-        return {'data': DATA}, 200
+    def post(self):
+        # parse request arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()# check if we already have the location in places list
+        if args['location'] in DATA['places']:
+            # if we do, return 401 bad request
+            return {
+                'message': f"'{args['location']}' already exists."
+                }, 401
+        else:
+            # otherwise, add the new location to places
+            DATA['places'].append(args['location'])
+            return {'data': DATA}, 200
+
+    def delete(self):
+        # parse request arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()# check if we have given location in places list
+        if args['location'] in DATA['places']:
+            # if we do, remove and return data with 200 OK
+            DATA['places'].remove(args['location'])
+            return {'data': DATA}, 200
+        else:
+            # if location does not exist in places list return 404
+            return {
+                'message': f"'{args['location']}' does not exist."
+                }, 404
+
+
+api.add_resource(Places, '/places')
+
+if __name__ == '__main__':
+    if (os.getenv('FLASK_DEV_ENV', None) is not None):
+        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     else:
-        # if location does not exist in places list return 404
-        return {
-            'message': f"'{args['location']}' does not exist."
-            }, 404
+        serve(app, host='0.0.0.0', port=5000)
