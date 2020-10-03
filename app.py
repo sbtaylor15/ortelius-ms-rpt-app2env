4d091c90eab392624485410b6dd9e4a7ba3a75f8
Sacha Wharton <sacha@gmail.com>
2020-10-03 16:33:22 +0200

    waitress config

--- app.py
+++ app.py
@@ -60,6 +60,6 @@ api.add_resource(Places, '/places')
 
 if __name__ == '__main__':
     if (os.getenv('FLASK_DEV_ENV', None) is not None):
-        app.run(app debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+        app.run(app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     else:
         serve(app, host='0.0.0.0', port=5000)
