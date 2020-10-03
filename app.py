b5a80f8b1d21abfa8c1be5f5bc8f4c4ca9681ad6
Sacha Wharton <sacha@gmail.com>
2020-10-03 19:39:35 +0200

    python type error

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from waitress import serve
+from app import app
 import os
 
 app = Flask(__name__)
@@ -59,6 +60,6 @@ api.add_resource(Places, '/places')
 
 if __name__ == '__main__':
     if (os.getenv('FLASK_DEV_ENV', None) is not None):
-        app.run(api, host='0.0.0.0', PORT=5000)
+        app.run(host='0.0.0.0', PORT=5000)
     else:
         serve(app, host='0.0.0.0', port=5000)
